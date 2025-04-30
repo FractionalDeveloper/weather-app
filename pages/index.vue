9cf0bea76662ab3425f00009288598423837d0d0
Rebar Hassan <mail@rebars.space>
2025-04-30 10:44:47 +0200

    Remove unused div in index page.

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,6 @@
     <div class="flex w-4/6 h-full flex-col space-y-4">
       <div class="flex bg-orange-400 w-full h-1/3 rounded-3xl"></div>
       <div class="flex bg-blue-400 w-full h-2/3  rounded-3xl"></div>
-      <div></div>
     </div>
     <div class="flex bg-pink-600 w-2/6 h-full rounded-3xl"></div>
   </div>
