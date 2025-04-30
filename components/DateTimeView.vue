e81f700e8f043341ed178ffc3d51c11383a75908
Rebar Hassan <mail@rebars.space>
2025-04-30 10:53:24 +0200

    DateTimeView component in index page.

---
+++ components/DateTimeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1 class="text-2xl text-center mt-3 font-bold w-full">{{new Date().toDateString()}}</h1>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 
+import DateTimeView from "~/components/DateTimeView.vue";
 </script>
 
 <template>
   <div class="flex flex-row w-full h-full space-x-5">
     <div class="flex w-4/6 h-full flex-col space-y-4">
-      <div class="flex bg-orange-400 w-full h-1/3 rounded-3xl"></div>
+      <div class="flex w-full h-1/3 rounded-3xl">
+        <DateTimeView/>
+      </div>
       <div class="flex bg-blue-400 w-full h-2/3  rounded-3xl"></div>
     </div>
     <div class="flex bg-pink-600 w-2/6 h-full rounded-3xl"></div>
